a815409d9991d8dcc259e96b61b7de1db333d9f7
Ramesh Choudhary <rchoudhary@accoonaglobal.com>
2023-09-26 12:16:37 +0530

    Problem:387-First Unique Character in a string

---
+++ LeetCode/Problems/387-FirstUniqChar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems;
+internal class _387_FirstUniqChar
+{
+    //Problem : https://leetcode.com/problems/first-unique-character-in-a-string/
+    //Solution : https://leetcode.com/problems/first-unique-character-in-a-string/submissions/1058893300/
+
+    public int FirstUniqChar(string s)
+    {
+        var dict = new Dictionary<char, int>();
+        foreach (char c in s)
+        {
+            if (dict.ContainsKey(c)) dict[c]++;
+            else dict[c] = 0;
+        }
+
+        foreach (var item in dict)
+        {
+            if (item.Value == 0) return s.IndexOf(item.Key);
+        }
+        return -1;
+    }
+}
