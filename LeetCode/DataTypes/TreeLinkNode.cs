162e6c74330ad281022fcc8f5a67c7f1eea50036
Ramesh Choudhary <rchoudhary@accoonaglobal.com>
2023-09-28 12:49:18 +0530

    Setup test project and required helpers and dataTypes Ref : https://github.com/BigEggStudy/LeetCode-CS/tree/master/LeetCode.Test

---
+++ LeetCode/DataTypes/TreeLinkNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.DataTypes;
+public class TreeLinkNode
+{
+    public int val;
+    public TreeLinkNode left;
+    public TreeLinkNode right;
+    public TreeLinkNode next;
+
+    public TreeLinkNode(int x) { val = x; }
+}
