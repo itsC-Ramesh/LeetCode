0cdb8bea3ac11afece0b2ee140f3a26a896a7e80
Ramesh Choudhary <itsc.ramesh@gmail.com>
2023-10-03 19:38:20 +0530

    Problem: 34-Find First and Last Position of Element in sorted Array

---
+++ LeetCode.Tests/Misc/34-FindFirstAndLastPositionOfElementInSortedArray-Test.cs
@@ -0,0 +1,59 @@
+﻿using LeetCode.Problems;
+
+namespace LeetCode.Test.Misc;
+[TestFixture]
+public class _34_FindFirstAndLastPositionOfElementInSortedArray_Test
+{
+    private _34_FindFirstAndLastPositionOfElementInSortedArray solution;
+    [SetUp]
+    public void Setup()
+    {
+        solution = new _34_FindFirstAndLastPositionOfElementInSortedArray();
+    }
+
+    [Test]
+    public void SearchRange_1()
+    {
+        int[] nums = { 5, 7, 7, 8, 8, 8, 10 };
+        int target = 8;
+        int[] expected = { 3, 5 };
+        Assert.That(solution.SearchRange(nums, target), Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void SearchRange_2()
+    {
+        int[] nums = { 5, 7, 7, 8, 8, 8, 10 };
+        int target = 6;
+        int[] expected = { -1, -1 };
+        Assert.That(solution.SearchRange(nums, target), Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void SearchRange_3()
+    {
+        int[] nums = { 1, 2, 3, 4, 5 };
+        int target = 3;
+        int[] expected = { 2, 2 };
+        Assert.That(solution.SearchRange(nums, target), Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void SearchRange_4()
+    {
+        int[] nums = { 1, 2, 2, 2, 3, 4, 5, 5, 5 };
+        int target = 5;
+        int[] expected = { 6, 8 };
+        Assert.That(solution.SearchRange(nums, target), Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void SearchRange_5()
+    {
+        int[] nums = { 1 };
+        int target = 1;
+        int[] expected = { 0, 0 };
+        Assert.That(solution.SearchRange(nums, target), Is.EqualTo(expected));
+    }
+
+}
---
+++ LeetCode/Problems/34-FindFirstAndLastPositionOfElementInSortedArray.cs
@@ -0,0 +1,79 @@
+﻿namespace LeetCode.Problems;
+public class _34_FindFirstAndLastPositionOfElementInSortedArray
+{
+    //Problem: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+    /*
+    Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+    If target is not found in the array, return [-1, -1].
+    You must write an algorithm with O(log n) runtime complexity.
+
+    Example 1:    
+        Input: nums = [5,7,7,8,8,10], target = 8
+        Output: [3,4]
+    
+    Example 2:    
+        Input: nums = [5,7,7,8,8,10], target = 6
+        Output: [-1,-1]
+    
+    Example 3:
+        Input: nums = [], target = 0
+        Output: [-1,-1]
+     
+    
+    Constraints:
+        0 <= nums.length <= 105
+        -109 <= nums[i] <= 109
+        nums is a non-decreasing array.
+        -109 <= target <= 109    
+     */
+
+    //Solution: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/submissions/1065917258/
+    public int[] SearchRange(int[] nums, int target)
+    {
+        int start = 0; int end = nums.Length - 1;
+
+        int i = -1; int j = -1;
+
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target)
+            {
+                i = mid;
+                end = mid - 1;
+            }
+            else if (nums[mid] > target)
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+
+        start = 0;
+        end = nums.Length - 1;
+
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target)
+            {
+                j = mid;
+                start = mid + 1;
+            }
+            else if (nums[mid] > target)
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        return new int[] { i, j };
+    }
+}
